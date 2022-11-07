14659d09996d6705ef173220fe76d9a499675fd2
seppukud <minhcoding18@gmail.com>
2022-11-06 22:55:03 -0600

    fix

--- app.py
+++ app.py
@@ -418,5 +418,6 @@ def parse(data):
     return ans
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
-
+    #app.run(host="0.0.0.0",debug=True)
+    
+    app.run(host="0.0.0.0", port=80 ,debug=True)
